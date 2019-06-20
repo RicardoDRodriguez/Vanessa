f8a8dfe4fa3f77f3cf0c0ef1becbc57c5636cf83
Ricardo Rodriguez <ricardo.domingos@gmail.com>
2019-06-20 18:12:19 -0300

    Retirado o controle de data string

--- dashboard/dateDashboard.js
+++ dashboard/dateDashboard.js
@@ -9,17 +9,6 @@ DateDashboard = function(date){
 	this.date = date;
 	this.error = this.date = isNaN || date == '';
 
-	this.converteData = function(){ //data no formato dd-mm-yyyy hh:mn:ss
-		if (this.error) {
-			return this.getMessageError();
-		}
-		var dateTimeParts = this.dateString.split(' ');
-		var timeParts = dateTimeParts[1].split(':');
-		var dateParts = dateTimeParts[0].split('-');
-		this.date =  
-			new Date(dateParts[2], parseInt(dateParts[1], 10) - 1, dateParts[0], timeParts[0], timeParts[1], timeParts[2]);
-	}
-
 	this.getMessageError = function(){
 		console.error("Data informada é nula: "+ dateString);
 		return null;
