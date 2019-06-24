1cdaf999f3d4b04c6ebbbadf5651686da800b8e6
Ricardo Rodriguez <ricardo.domingos@gmail.com>
2019-06-23 21:28:38 -0300

    Página de index criada. Falta incluir os graficos.

--- index.js
+++ index.js
@@ -5,7 +5,22 @@
 const express = require ("express");
 const app = express() //  cria a aplicação.
 
+app.get('/', function(req,res){
+	res.sendFile(__dirname +"/html/index.html");
+	
+	/*--------------------------------------------------
+	 * Rodar o looping dos graficos de forma assíncrona.
+	 * -------------------------------------------------
+	 */
+	
+	
+	
+});
 
+//-------------------
+//Executa a aplicação
+//-------------------
 
-
-app.listen('1732');
+app.listen(1732, function(){
+	console.log("Servidor rodando na url: http://localhost:1732");
+});
